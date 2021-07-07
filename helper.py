6a5f2a42501f220b80474dcb319a695b2bb8c3e9
Bart92 <bart-driessen@hotmail.com>
2021-07-07 10:31:36 +0200

    Update helper.py
    removing if elifs and putting index functions in dictionary

--- helper.py
+++ helper.py
@@ -28,28 +28,24 @@ ndre1 = lambda b5,b8: (b8 - b5) / (b8 + b5)
 ndre2 = lambda b6,b8: (b8 - b6) / (b8 + b6)
 ndre5 = lambda b5,b7: (b7 - b5) / (b7 + b5)
 
+indices = {
+	"NDVI": ndvi,
+	"NDMI": ndmi,
+	"NDGI": ndgi,
+	"ANIR": anir,
+	"NDRE1": ndre1,
+	"NDRE2": ndre2,
+	"NDRE5": ndre5
+}
+
+def callback(x: ProcessBuilder, index, lenx, nlenx):
+	return array_modify(data=x, values=indices[index](*[x.array_element(i) for i in range(lenx)]), index=nlenx)
 
 def compute_features(datacube, features:list):
 	dc = datacube
 	lenx = len(datacube.metadata._band_dimension.bands)
 	nlenx = lenx
 	for i in features:
-		if i=="NDVI":
-			dc = dc.apply_dimension(dimension="bands",process=lambda x: array_modify(data=x, values=ndvi(*[x.array_element(i) for i in range(lenx)]), index=nlenx))
-		elif i=="NDMI":
-			dc = dc.apply_dimension(dimension="bands",process=lambda x: array_modify(data=x, values=ndmi(*[x.array_element(i) for i in range(lenx)]), index=nlenx))
-		elif i=="NDGI":
-			dc = dc.apply_dimension(dimension="bands",process=lambda x: array_modify(data=x, values=ndgi(*[x.array_element(i) for i in range(lenx)]), index=nlenx))
-		elif i=="ANIR":
-			dc = dc.apply_dimension(dimension="bands",process=lambda x: array_modify(data=x, values=anir(*[x.array_element(i) for i in range(lenx)]), index=nlenx))
-		elif i=="NDRE1":
-			dc = dc.apply_dimension(dimension="bands",process=lambda x: array_modify(data=x, values=ndre1(*[x.array_element(i) for i in range(lenx)]), index=nlenx))
-		elif i=="NDRE2":
-			dc = dc.apply_dimension(dimension="bands",process=lambda x: array_modify(data=x, values=ndre2(*[x.array_element(i) for i in range(lenx)]), index=nlenx))
-		elif i=="NDRE5":
-			dc = dc.apply_dimension(dimension="bands",process=lambda x: array_modify(data=x, values=ndre5(*[x.array_element(i) for i in range(lenx)]), index=nlenx))
-		else:
-			raise NotImplementedError("The index "+i+" has not yet been implemented. PLease choose between NDVI, NDMI, NDGI, ANIR, NDRE1, NDRE2 or NDRE5")
+		dc = dc.apply_dimension(dimension="bands", process=lambda x: callback(x,i,lenx,nlenx))
 		nlenx += 1
 	return dc
-
