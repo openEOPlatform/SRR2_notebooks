319c0bc9bf95ac46257b272999abc288a01e8bf3
DRIESSEB <bart.driessen@vito.be>
2021-07-07 17:16:11 +0200

    rewrote ANIR function to accept scalar processbuilder instance inputs

--- helper.py
+++ helper.py
@@ -4,28 +4,32 @@ Module for calculating a list of vegetation indices from a datacube containing b
 
 import openeo
 from openeo.rest.datacube import DataCube
-from openeo.processes import ProcessBuilder, array_modify, array_concat
+from openeo.processes import ProcessBuilder, array_modify, array_concat, array_apply, power, sqrt, clip, if_, multiply, divide, arccos
+import openeo.processes as prc
 import numpy as np
 
 WL_B04 = 0.6646
 WL_B08 = 0.8328
 WL_B11 = 1.610
+one_over_pi = 1./np.pi
 
 # source: https://git.vito.be/projects/LCLU/repos/satio/browse/satio/rsindices.py
 ndvi = lambda B04,B08: (B08 - B04) / (B08 + B04) 
 ndmi = lambda B08,B11: (B08 - B11) / (B08 + B11)
 ndgi = lambda B03,B04: (B03 - B04) / (B03 + B04)
+
 def anir(B04, B08, B11):
-    a = np.sqrt(np.square(WL_B08 - WL_B04) + np.square(B08 - B04))
-    b = np.sqrt(np.square(WL_B11 - WL_B08) + np.square(B11 - B08))
-    c = np.sqrt(np.square(WL_B11 - WL_B04) + np.square(B11 - B04))
+	a = sqrt(np.square(WL_B08 - WL_B04) + power(B08 - B04, 2))
+	b = sqrt(np.square(WL_B11 - WL_B08) + power(B11 - B08, 2))
+	c = sqrt(np.square(WL_B11 - WL_B04) + power(B11 - B04, 2))
 
-    # calculate angle with NIR as reference (ANIR)
-    site_length = (np.square(a) + np.square(b) - np.square(c)) / (2 * a * b)
-    site_length[site_length < -1] = -1
-    site_length[site_length > 1] = 1
+	# calculate angle with NIR as reference (ANIR)
+	site_length = (power(a,2) + power(b,2) - power(c,2)) / (2 * a * b)
+	site_length = if_(site_length.lt(-1),-1,site_length)
+	site_length = if_(site_length.gt(1),1,site_length)
 
-    return 1. / np.pi * np.arccos(site_length)
+	return multiply(one_over_pi, arccos(site_length))
+
 ndre1 = lambda B05,B08: (B08 - B05) / (B08 + B05)
 ndre2 = lambda B06,B08: (B08 - B06) / (B08 + B06)
 ndre5 = lambda B05,B07: (B07 - B05) / (B07 + B05)
@@ -45,7 +49,7 @@ def _callback(x: ProcessBuilder, index_list:list, datacube:DataCube) -> ProcessB
 	tot = x
 	for idx in index_list:
 		if idx not in indices.keys(): raise NotImplementedError("Index "+idx+" has not been implemented.")
-		band_indices = [datacube.metadata.get_band_index(band) for band in indices[idx].__code__.co_varnames]
+		band_indices = [datacube.metadata.get_band_index(band) for band in indices[idx].__code__.co_varnames[:indices[idx].__code__.co_argcount]]
 		lenx += 1
 		tot = array_modify(data=tot,values=indices[idx](*[tot.array_element(i) for i in band_indices]),index=lenx)
 	return tot
@@ -61,3 +65,20 @@ def compute_indices(datacube:DataCube, index_list:list) -> DataCube:
 
 	"""
 	return datacube.apply_dimension(dimension="bands", process=lambda x: _callback(x,index_list,datacube)).rename_labels('bands', target=datacube.metadata.band_names+index_list)
+
+
+bbox = (4.73382, 50.795237, 4.734988, 50.795519)
+# connection = openeo.connect("https://openeo-dev.vito.be").authenticate_oidc()
+# s2 = connection.load_collection("TERRASCOPE_S2_TOC_V2", 
+#                                 spatial_extent={'west':bbox[0],'east':bbox[2],'south':bbox[1],'north':bbox[3]}, 
+#                                 temporal_extent=["2018-01-01", "2018-01-04"], 
+#                                 bands=["B04","B08","B11"])
+
+connection = openeo.connect("https://openeo-dev.vito.be").authenticate_oidc()
+s2 = connection.load_collection("TERRASCOPE_S2_TOC_V2", 
+                                spatial_extent={'west':bbox[0],'east':bbox[2],'south':bbox[1],'north':bbox[3]}, 
+                                temporal_extent=["2018-01-01", "2019-01-01"], 
+                                bands=["B04","B08","B11","SCL"])
+s2_masked = s2.process("mask_scl_dilation", data=s2, scl_band_name="SCL").filter_bands(["B04","B08","B11"])
+feats = compute_indices(s2_masked, ["ANIR","NDVI"])
+feats.download("./data/feats.tif", format="GTiff")
